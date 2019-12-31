49672c3e08089b364bd679d50074e31b948bd9e3
chavonac <chavonbassguitar@hotmail.com>
2019-12-30 23:35:45 -0600

    primer commit

---
+++ src/test/java/mx/com/lobos/SpringbootServicioZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.lobos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
