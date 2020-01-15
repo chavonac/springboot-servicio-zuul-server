c7a68b79aa62d55288ef4af53fee042937b30e25
chavonac <chavonbassguitar@hotmail.com>
2020-01-15 00:25:05 -0600

    seccion 7 video 75

--- src/main/java/mx/com/lobos/oauth/ResourceServeConfig.java
+++ src/main/java/mx/com/lobos/oauth/ResourceServeConfig.java
@@ -1,5 +1,7 @@
 package mx.com.lobos.oauth;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,10 +12,14 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 @EnableResourceServer
 public class ResourceServeConfig extends ResourceServerConfigurerAdapter{
 
+	@Value("${config.security.oauth.jwt.key}")
+	private String jwtKey;
+	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/api/security/oauth/**").permitAll()
@@ -39,7 +45,7 @@ public class ResourceServeConfig extends ResourceServerConfigurerAdapter{
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("algun_codigo_secreto_aeiou");
+		tokenConverter.setSigningKey(jwtKey);
 		return tokenConverter;
 	}
 
